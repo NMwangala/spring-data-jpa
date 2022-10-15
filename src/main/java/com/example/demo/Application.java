2270c329ac2a982fbd7e8b367fdc10c3dbb9c0a3
nmwangala <mwangalanelson@gmail.com>
2022-10-15 13:59:08 +0300

    Added readme

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,56 @@
 package com.example.demo;
 
+import java.util.List;
+
+import org.assertj.core.util.Arrays;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+		return args -> {
+			Student maria = new Student("maria.jones@gmail.com", "Maria", "Jones", 20);
+			Student ahmed = new Student("ahmed.ali@gmail.com", "Ahmed", "Ali", 16);
+			
+			System.out.println("Adding students");
+			studentRepository.saveAll(List.of(maria, ahmed));
+
+			System.out.println("Number of students");
+			System.out.println(studentRepository.count());
+
+			System.out.println("Fetching students");
+			studentRepository.findById(2L).ifPresentOrElse(student -> {
+				System.out.println(student);
+			}, () -> System.out.println("Student with ID 2 not found"));
+			
+			
+			studentRepository.findById(3L).ifPresentOrElse(student -> {
+				System.out.println(student);
+			}, () -> System.out.println("Student with ID 3 not found"));
+			
+			System.out.println("Fetching all students");
+			List<Student> student = studentRepository.findAll();
+			
+			student.forEach(System.out::println);
+			
+			studentRepository.deleteById(1L);
+			System.out.println("Number of students");
+			System.out.println(studentRepository.count());
+			
+			System.out.println("=================");
+			
+			studentRepository.findStudentByEmail("ahmed.ali@gmail.com").ifPresentOrElse(System.out::println, ()-> 
+				
+			System.out.println("Student with ID 3 not found"));
+		};
+	}
 }
---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,89 @@
+package com.example.demo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "student", uniqueConstraints = { @UniqueConstraint(columnNames = "email", name = "student_email_unique") })
+public class Student {
+
+	@Id
+	@SequenceGenerator(name = "student_sequence", sequenceName = "student_sequency", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
+	@Column(name = "id", updatable = false)
+	private Long id;
+	@Column(name = "email", columnDefinition = "TEXT", nullable = false)
+	private String email;
+
+	@Column(name = "first_name", nullable = false, columnDefinition = "TEXT")
+	private String firstName;
+	@Column(name = "last_name", nullable = false, columnDefinition = "TEXT")
+	private String lastName;
+
+	@Column(name = "age", nullable = false)
+	private Integer age;
+
+	public Student() {
+
+	}
+
+	public Student( String email, String firstName, String lastName, Integer age) {
+		this.email = email;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", email=" + email + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", age=" + age + "]";
+	}
+
+}
